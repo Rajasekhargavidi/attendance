b28d0388de054dd1ecc3d724b61d29f178791919
rajasekhar <rajasekhar.gavidi@gmail.com>
2019-06-29 10:21:20 +0530

    authentication

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,26 +2,51 @@ import React, { Component } from "react";
 import { Avatar } from "antd";
 import { Form, Icon, Input, Button, Checkbox } from "antd";
 import { NavLink, Link } from "react-router-dom";
+import { withFirebase } from "../firebase";
+
+const INITIAL_STATE = {
+  email: "",
+  password: "",
+  error: null
+};
 
 class Form1 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
         console.log("Received values of form: ", values);
+        this.props.firebase
+          .doSignInWithEmailAndPassword(values.email, values.password)
+          .then(authUser => {
+            this.setState({ ...INITIAL_STATE });
+            console.log(authUser);
+            this.props.history.push("/DashBoard");
+          })
+          .catch(error => {
+            console.log(error);
+            this.setState({ error });
+          });
       }
     });
   };
 
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const { getFieldDecorator } = this.props.form; 
+    const { error } = this.state;
     return (
       <div className="form-layout">
         <Avatar size={100} icon="user" />
         <h2>Sign In</h2>
         <Form onSubmit={this.handleSubmit} className="login-form">
           <Form.Item>
-            {getFieldDecorator("username", {
+            {getFieldDecorator("email", {
               rules: [
                 { required: true, message: "Please input your username!" }
               ]
@@ -64,17 +89,20 @@ class Form1 extends Component {
               htmlType="submit"
               className="login-form-button"
             >
-              <NavLink to="/Dashboard">Sign In</NavLink>
+              Sign In
               {"     "}
             </Button>
             Or {"  "}
             <Link to="SignUp">Register now!</Link>
+            {error && <p>{error.message}</p>}
           </Form.Item>
         </Form>
       </div>
     );
   }
 }
-const SignIn = Form.create({ name: "normal_login" })(Form1);
+const SignInForm = Form.create({ name: "normal_login" })(Form1);
+
+const SignIn = withFirebase(SignInForm);
 
 export default SignIn;
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import app from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyC-YLp8BHqVxLEkqsJdpLHH5a29vJFmTWY",
-  authDomain: "teacher-portal-451eb.firebaseapp.com",
-  databaseURL: "https://teacher-portal-451eb.firebaseio.com",
-  projectId: "teacher-portal-451eb",
-  storageBucket: "",
-  messagingSenderId: "468979536607",
-  appId: "1:468979536607:web:fed79485eac4c3e9"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 class Firebase {
